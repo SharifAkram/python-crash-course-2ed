b5aab133beb1b4897e244f9dc08cb6a4d6d3b157
Sharif Akram <95551454+SharifAkram@users.noreply.github.com>
2023-06-18 17:54:10 -0400

    Update t-shirt.py

--- t-shirt.py
+++ t-shirt.py
@@ -3,5 +3,5 @@ def make_shirt(size, message):
 	print(f"\nYou requested a {size} t-shirt.")
 	print(f"{message.title()} will be printed on your t-shirt.")
 
-# make_shirt('medium', 'pro-choice')
+# make_shirt('medium', 'Jamaica')
 make_shirt(message='USA', size='large')
